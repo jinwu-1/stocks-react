f5c03840e910fa13bd390640cc55c062cdcd63db
Jin Wu <jinn.wuu@gmail.com>
2021-05-03 12:53:19 -0400

    Navbar

---
+++ src/components/pages/SignUp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class SignUp extends React.Component {
+    render() {
+        return (
+            <h1>Sign Up Page</h1>
+        )
+    }
+}
+
+export default SignUp
